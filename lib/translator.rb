d8973bd7e5583430d6e09ca74c51ed545c90afde
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-18 05:05:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,9 @@ def load_library(path)
     emoticon_lib.each do |key,value|
       eng=value[0]
       jpn=value[1]
-
+      new_hash[:english]=eng
+      new_hash[:japanese]=jpn
+new_hash
 
 
     binding.pry
