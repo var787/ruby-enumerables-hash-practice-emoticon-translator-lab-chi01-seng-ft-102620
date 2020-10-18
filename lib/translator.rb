1ea4a527c1223c1a2abd8d1078897fc91e28e81f
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-18 04:58:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,8 @@ require 'pry'# require modules here
 def load_library(path)
     emoticon_lib=YAML.load_file(path)
     emoticon_names=emoticon_lib.keys
-    new_hash.push[emoticon_names]
+    new_hash={}
+    [emoticon_names]
     
 
     binding.pry
