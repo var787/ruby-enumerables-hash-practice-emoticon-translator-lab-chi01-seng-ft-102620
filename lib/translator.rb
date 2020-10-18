c5025116a4ac68403bbdf85fa20e96f91f64665a
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-18 05:30:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,8 +17,8 @@ end
 def get_english_meaning(path,emoticon)
     load_library(path).each do |key,value|
       #binding.pry
-if value[:english]
-  return value[:japanese]
+   value[:english]
+   value[:japanese]
   binding.pry
 
     end
