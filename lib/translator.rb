bbbbadf93b2fae529607b80944be162ea84ee743
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-18 04:49:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(path)
     emoticon_lib=YAML.load_file(path)
     emoticon_lib.each do |key,value|
       my_hash={}
-      my_hash{key}
+      
       eng=value[0]
       jpn=value[1]
       #binding.pry
